85b8484c35b0be6d9804bb324be5c866c73e7676
adojem <adojemcovix@gmail.com>
2018-08-06 05:45:03 +0900

    Get & Populate Items

--- app.js
+++ app.js
@@ -1,48 +1,71 @@
 // Storage Controller
 
 // Item Controller
-const ItemCtrl = (function () {
+const ItemCtrl = (function() {
    // Item Constructor
-   const Item = function (id, name, calories) {
+   const Item = function(id, name, calories) {
       this.id = id;
       this.name = name;
       this.calories = calories;
-   }
+   };
 
    // Data Structure / State
    const data = {
       item: [
          { id: 0, name: 'Steak Dinner', calories: 1200 },
          { id: 1, name: 'Cookie', calories: 400 },
-         { id: 2, name: 'Eggs', calories: 300 },
+         { id: 2, name: 'Eggs', calories: 300 }
       ],
       currentItem: null,
-      totalCalories: 0,
+      totalCalories: 0
    };
 
    return {
-      logData: function () {
+      getItems: function() {
+         return data.item;
+      },
+      logData: function() {
          return data;
       }
    };
 })();
 
-
 // UI Controller
 const UICtrl = (function () {
-   
-})();
-
-
-// App Controller
-const App = (function (ItemCtrl, UICtrl) {
+   const UISelector = {
+      itemList: '#item-list',
+   };
 
    return {
-      init: function () {
-         console.log('Initializing App...');
+      populateItemList: function(items) {
+         let html = '';
+
+         items.forEach(function(item) {
+            html += `<li class="collection-item" id="item-${item.id}">
+                  <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                  <a href="#" class="secondary-content">
+                     <i class="edit-item fa fa-pencil"></i>
+                  </a>
+               </li>`;
+         });
+
+         // Insert list item
+         document.querySelector(UISelector.itemList).innerHTML = html;
       }
    };
-   
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+   return {
+      init: function() {
+         // Fetch items from data structure
+         const items = ItemCtrl.getItems();
+
+         // Populate list with items
+         UICtrl.populateItemList(items);
+      }
+   };
 })(ItemCtrl, UICtrl);
 
 App.init();
